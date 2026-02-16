bc4abf9ffb28d2e1417c2ec350519b2fe6ea1596
sabbir53rahman <sabbir53rahman@gmail.com>
2026-02-16 07:27:01 +0600

    update global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,6 +2,12 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import z from "zod";
+
+interface TErrorSources {
+  path: string;
+  message: string;
+}
 
 export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,13 +20,28 @@ export const globalErrorHandler = (
     console.log("Error from global error handler", err);
   }
 
-  const statusCode: number = status.INTERNAL_SERVER_ERROR;
+  const errorSource: TErrorSources[] = [];
 
-  const message: string = "Internal Server Error";
+  let statusCode: number = status.INTERNAL_SERVER_ERROR;
+
+  let message: string = "Internal Server Error";
+
+  if (err instanceof z.ZodError) {
+    statusCode = status.BAD_REQUEST;
+    message = "Validation Error";
+
+    err.issues.forEach((issue) => {
+      errorSource.push({
+        path: issue.path.join(".") || "unknown",
+        message: issue.message,
+      });
+    });
+  }
 
   res.status(statusCode).json({
     success: false,
     message: message,
     error: err.message,
+    errorSource,
   });
 };
